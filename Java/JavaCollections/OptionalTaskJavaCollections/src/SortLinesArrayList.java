d55122f0da303f498ec6f8c3755b6d92ef378b7b
Dmitry <53450936+Dmitr3@users.noreply.github.com>
2021-02-27 20:50:26 +0300

    Add files via upload

---
+++ Java/JavaCollections/OptionalTaskJavaCollections/src/SortLinesArrayList.java
@@ -0,0 +1,27 @@
+// Ввести строки из файла, записать в список ArrayList.
+// Выполнить сортировку строк, используя метод sort() из класса Collections.
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class SortLinesArrayList {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("test.txt"));
+        ArrayList<String> arrStr = new ArrayList<>();
+
+        while (scan.hasNext()) {
+            arrStr.add(scan.nextLine());
+        }
+
+        scan.close();
+        Collections.sort(arrStr);
+
+        for (String anArrStr : arrStr) {
+            System.out.println(anArrStr);
+        }
+    }
+}
